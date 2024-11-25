594b0aed782bdb7921828fbd241f4d0a3e543a98
Frederick Lawson <freddie@corneliajames.com>
2024-11-25 15:49:25 -0400

    SC copy

--- app/returns/[id]/components/StoreCredit.js
+++ app/returns/[id]/components/StoreCredit.js
@@ -1,16 +1,27 @@
+import Link from "next/link";
+
 export default function StoreCredit({ returnData }){
   
   return (
     <>
-      <div className="container bg-white rounded-lg p-5 md:px-8 md:pb-8 space-y-4">
-        <h2 className="heading-secondary">Store Credit</h2>
-        <div className="flex flex-col gap-4 items-center">
-          <span className="text-sm text-center">
+      <div className="container bg-white rounded-lg p-5 md:p-8 space-y-4 flex flex-col items-center text-center gap-4">
+        <div className="">
+          <h2 className="heading-secondary">Store Credit</h2>
+          <span className="">
             Your store credit has been automatically issued for the account associated with the email <span className="font-bold">{returnData.email}</span>.
-            You should have received an email with the details. If there are any issues, please contact us.
+            You should have received an email with the details.
           </span>
+        </div>
+          
+        <div className="flex flex-col gap-2">
+          <h3 className="text-lg font-light">Store Credit Hasn't Arrived?</h3>
+          <span>If you don't receive an email notifying you of your store credit in a couple of minutes, click below to view your account where you should see it. Alternatively, please contact us.</span>
+        </div>
+        <div className="flex gap-4">
+          <Link href="https://account.corneliajames.com/profile" className="btn btn-primary">View Account</Link>
           <button className="btn btn-tertiary">Contact Us</button>
         </div>
+        
       </div>
     </>
   )
